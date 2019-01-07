628cdff74943507a6848a705c3348e698ca3ace5
Mathieu Tadeo <46216783+Mathiieeeu@users.noreply.github.com>
2019-01-07 13:44:56 +0800

    Update videooverlay.js

--- CarloCalmaConsultancyInc/js/videooverlay.js
+++ CarloCalmaConsultancyInc/js/videooverlay.js
@@ -1,4 +1,25 @@
-$(document).ready(function(){
+sessionStorage.setItem('firstVisit', '0');
+
+if(sessionStorage.getItem('firstVisit') === '0')
+{ 
+    $('#video, #overlay').fadeIn('slow');
+    $('#video').html('<div class="col-md-6"><img class="img-fluid" src="images/carlocalma_logostudies.png"></div>');
+    $(document).on("click", function (e){
+        if($('#overlay').is(e.target))
+        {
+            $('#video, #overlay').fadeOut('slow');
+            $('#video').html('');
+            sessionStorage.setItem('firstVisit', '1');
+        }
+    });
+}
+else
+{
+}
+
+
+
+/*$(document).ready(function(){
     $('#video, #overlay').fadeIn('slow');
     $('#video').html('<div class="col-md-6"><img class="img-fluid" src="images/carlocalma_logostudies.png"></div>');
 });
