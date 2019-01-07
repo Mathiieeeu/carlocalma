253f67b08e4667be5af5783f9b8f09066646ed44
Mathieu Tadeo <46216783+Mathiieeeu@users.noreply.github.com>
2019-01-07 14:19:59 +0800

    Update videooverlay.js

--- CarloCalmaConsultancyInc/js/videooverlay.js
+++ CarloCalmaConsultancyInc/js/videooverlay.js
@@ -1,34 +1,18 @@
-/*$(document).ready(function(){
-    
-    sessionStorage.setItem('firstVisit', '0');
+sessionStorage.setItem('firstVisit', '0');
 
-    if(sessionStorage.getItem('firstVisit') === '0')
-    { 
-        $('#video, #overlay').fadeIn('slow');
-        $('#video').html('<div class="col-md-6"><img class="img-fluid" src="images/carlocalma_logostudies.png"></div>');
-        $(document).on("click", function (e){
-            if($('#overlay').is(e.target))
-            {
-                $('#video, #overlay').fadeOut('slow');
-                $('#video').html('');
-                sessionStorage.setItem('firstVisit', '1');
-            }
-        });
-    }
-    else
-    {
-    }
-    
-});*/
-
-$(document).ready(function(){
+if(sessionStorage.getItem('firstVisit') === '0')
+{ 
     $('#video, #overlay').fadeIn('slow');
     $('#video').html('<div class="col-md-6"><img class="img-fluid" src="images/carlocalma_logostudies.png"></div>');
-});
-
-$(document).on("click", function (e){
-    if($('#overlay').is(e.target)){
-        $('#video, #overlay').fadeOut('slow');
-        $('#video').html('');
-    }
-});
+    $(document).on("click", function (e){
+        if($('#overlay').is(e.target))
+        {
+            $('#video, #overlay').fadeOut('slow');
+            $('#video').html('');
+            sessionStorage.setItem('firstVisit', '1');
+        }
+    });
+}
+else
+{
+}
