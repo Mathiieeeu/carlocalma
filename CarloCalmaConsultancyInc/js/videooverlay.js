7aba2c6ec2dfaf6ebbfd09d46ec08f8c34d64a8c
Mathieu Tadeo <46216783+Mathiieeeu@users.noreply.github.com>
2019-01-07 13:28:00 +0800

    Update videooverlay.js

--- CarloCalmaConsultancyInc/js/videooverlay.js
+++ CarloCalmaConsultancyInc/js/videooverlay.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('#video, #overlay').fadeIn('slow');
-    $('#video').html('<div class="col-md-8"><img class="img-fluid" src="images/carlocalma_logostudies.png"></div>');
+    $('#video').html('<div class="col-md-6"><img class="img-fluid" src="images/carlocalma_logostudies.png"></div>');
 });
 
 $(document).on("click", function (e){
