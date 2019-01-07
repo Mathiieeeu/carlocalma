f4427e929a6c9415d35ab9c2a9c4049b5454b4ac
Mathieu Tadeo <46216783+Mathiieeeu@users.noreply.github.com>
2019-01-07 13:48:17 +0800

    Update videooverlay.js

--- CarloCalmaConsultancyInc/js/videooverlay.js
+++ CarloCalmaConsultancyInc/js/videooverlay.js
@@ -1,21 +1,25 @@
-sessionStorage.setItem('firstVisit', '0');
+$(document).ready(function(){
+    
+    sessionStorage.setItem('firstVisit', '0');
 
-if(sessionStorage.getItem('firstVisit') === '0')
-{ 
-    $('#video, #overlay').fadeIn('slow');
-    $('#video').html('<div class="col-md-6"><img class="img-fluid" src="images/carlocalma_logostudies.png"></div>');
-    $(document).on("click", function (e){
-        if($('#overlay').is(e.target))
-        {
-            $('#video, #overlay').fadeOut('slow');
-            $('#video').html('');
-            sessionStorage.setItem('firstVisit', '1');
-        }
-    });
-}
-else
-{
-}
+    if(sessionStorage.getItem('firstVisit') === '0')
+    { 
+        $('#video, #overlay').fadeIn('slow');
+        $('#video').html('<div class="col-md-6"><img class="img-fluid" src="images/carlocalma_logostudies.png"></div>');
+        $(document).on("click", function (e){
+            if($('#overlay').is(e.target))
+            {
+                $('#video, #overlay').fadeOut('slow');
+                $('#video').html('');
+                sessionStorage.setItem('firstVisit', '1');
+            }
+        });
+    }
+    else
+    {
+    }
+    
+});
 
 
 
